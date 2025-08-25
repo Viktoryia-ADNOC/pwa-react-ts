989e09adf3a31272feb4bb769cf7cb6ad1bae9b2
Viktoryia.Makaranka <viktoryia_makaranka@epam.com>
2025-08-25 17:05:47 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,43 @@
+// const POLLING_INTERVAL = 30000; // Check every 30 seconds
+//
+// // Polling mechanism
+// const startPolling = () => {
+//   const interval = setInterval(async () => {
+//     try {
+//       const response = await Promise.resolve({
+//         ok: true,
+//         json: () => Promise.resolve({ newUpdate: !!localStorage.getItem('updated') })
+//       });
+//
+//       console.log(localStorage.getItem('updated'));
+//
+//       if (response.ok) {
+//         const data = await response.json();
+//
+//         // Check condition from polling data
+//         if (data.newUpdate) {
+//           self.registration.showNotification('Update Available', {
+//             body: 'A new update is available. Click to view!',
+//             icon: '/logo192.png',
+//             data: { url: '/' }
+//           });
+//           localStorage.setItem('updated', '');
+//         }
+//       } else {
+//         console.error('Failed to fetch updates');
+//       }
+//     } catch (error) {
+//       console.error('Error during polling:', error);
+//       clearInterval(interval); // Stop polling on error
+//     }
+//   }, POLLING_INTERVAL);
+// };
+//
+// // Start polling when the service worker is activated
+// self.addEventListener('activate', (event) => {
+//   event.waitUntil(startPolling());
+// });
+
 self.addEventListener('push', (event) => {
   const data = event.data?.json() || {};
   const title = data.title || 'Notification';
@@ -28,4 +68,15 @@ self.addEventListener('notificationclick', (event) => {
       }
     })
   );
+});
+
+self.addEventListener('message', event => {
+  if (event.data && event.data.type === 'DATA_FROM_CLIENT') {
+    console.log('Received data from client:', event.data.payload);
+    self.registration.showNotification('Update Available', {
+      body: 'A new update is available. Click to view!',
+      icon: '/logo192.png',
+      data: { url: '/' }
+    });
+  }
 });
